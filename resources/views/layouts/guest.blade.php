c3fbf5499e65f1e083ea0e2eb82a99fd6a27c113
Will <twitchifygamers@gmail.com>
2022-05-03 19:04:42 +0100

    Added alpinejs to guest layout

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,6 +15,7 @@
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="//unpkg.com/alpinejs" defer></script>
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
