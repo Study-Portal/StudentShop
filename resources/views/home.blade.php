80c23dcdfccda526a591487c6b73ee18d0e3c2bb
Will <twitchifygamers@gmail.com>
2022-05-03 20:06:45 +0100

    Mobile menu wip

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-    <div class="bg-white">
+    <div class="bg-white" x-data="{menu: false}">
         <!--
           Mobile menu
 
@@ -29,9 +29,9 @@
                     From: "translate-x-0"
                     To: "-translate-x-full"
                 -->
-                <div class="relative max-w-xs w-full bg-white shadow-xl pb-12 flex flex-col overflow-y-auto">
+                <div class="relative max-w-xs w-full bg-white shadow-xl pb-12 flex flex-col overflow-y-auto" x-show="menu" x-cloak>
                     <div class="px-4 pt-5 pb-2 flex">
-                        <button type="button" class="-m-2 p-2 rounded-md inline-flex items-center justify-center text-gray-400">
+                        <button @click="menu = false" type="button" class="-m-2 p-2 rounded-md inline-flex items-center justify-center text-gray-400">
                             <span class="sr-only">Close menu</span>
                             <!-- Heroicon name: outline/x -->
                             <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
@@ -45,10 +45,10 @@
                         <div class="border-b border-gray-200">
                             <div class="-mb-px flex px-4 space-x-8" aria-orientation="horizontal" role="tablist">
                                 <!-- Selected: "text-indigo-600 border-indigo-600", Not Selected: "text-gray-900 border-transparent" -->
-                                <button @click="women = true; men = false" id="tabs-1-tab-1" class="text-gray-900 border-transparent flex-1 whitespace-nowrap py-4 px-1 border-b-2 text-base font-medium" aria-controls="tabs-1-panel-1" role="tab" type="button">Women</button>
+                                <button @click="women = true; men = false" id="tabs-1-tab-1" class="text-gray-900 border-transparent flex-1 whitespace-nowrap py-4 px-1 border-b-2 text-base font-medium" aria-controls="tabs-1-panel-1" role="tab" type="button">Education</button>
 
                                 <!-- Selected: "text-indigo-600 border-indigo-600", Not Selected: "text-gray-900 border-transparent" -->
-                                <button @click="men = true; women = false" id="tabs-1-tab-2" class="text-gray-900 border-transparent flex-1 whitespace-nowrap py-4 px-1 border-b-2 text-base font-medium" aria-controls="tabs-1-panel-2" role="tab" type="button">Men</button>
+                                <button @click="men = true; women = false" id="tabs-1-tab-2" class="text-gray-900 border-transparent flex-1 whitespace-nowrap py-4 px-1 border-b-2 text-base font-medium" aria-controls="tabs-1-panel-2" role="tab" type="button">Products</button>
                             </div>
                         </div>
 
@@ -153,48 +153,21 @@
 
                     <div class="border-t border-gray-200 py-6 px-4 space-y-6">
                         <div class="flow-root">
-                            <a href="#" class="-m-2 p-2 block font-medium text-gray-900">Company</a>
+                            <a href="#" class="-m-2 p-2 block font-medium text-gray-900">Support</a>
                         </div>
 
                         <div class="flow-root">
-                            <a href="#" class="-m-2 p-2 block font-medium text-gray-900">Stores</a>
+                            <a href="#" class="-m-2 p-2 block font-medium text-gray-900">About</a>
                         </div>
                     </div>
 
                     <div class="border-t border-gray-200 py-6 px-4 space-y-6">
                         <div class="flow-root">
-                            <a href="#" class="-m-2 p-2 block font-medium text-gray-900">Create an account</a>
+                            <a href="{{route('register')}}" class="-m-2 p-2 block font-medium text-gray-900">Create an account</a>
                         </div>
                         <div class="flow-root">
-                            <a href="#" class="-m-2 p-2 block font-medium text-gray-900">Sign in</a>
+                            <a href="{{route('login')}}" class="-m-2 p-2 block font-medium text-gray-900">Sign in</a>
                         </div>
-                    </div>
-
-                    <div class="border-t border-gray-200 py-6 px-4 space-y-6">
-                        <!-- Currency selector -->
-                        <form>
-                            <div class="inline-block">
-                                <label for="mobile-currency" class="sr-only">Currency</label>
-                                <div class="-ml-2 group relative border-transparent rounded-md focus-within:ring-2 focus-within:ring-white">
-                                    <select id="mobile-currency" name="currency" class="bg-none border-transparent rounded-md py-0.5 pl-2 pr-5 flex items-center text-sm font-medium text-gray-700 group-hover:text-gray-800 focus:outline-none focus:ring-0 focus:border-transparent">
-                                        <option>CAD</option>
-
-                                        <option>USD</option>
-
-                                        <option>AUD</option>
-
-                                        <option>EUR</option>
-
-                                        <option>GBP</option>
-                                    </select>
-                                    <div class="absolute right-0 inset-y-0 flex items-center pointer-events-none">
-                                        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" class="w-5 h-5 text-gray-500">
-                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6 8l4 4 4-4" />
-                                        </svg>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
                     </div>
                 </div>
             </div>
@@ -211,41 +184,6 @@
             <!-- Navigation -->
             <header class="relative z-10">
                 <nav aria-label="Top">
-                    <!-- Top navigation -->
-                    <div class="bg-gray-900">
-                        <div class="max-w-7xl mx-auto h-10 px-4 flex items-center justify-between sm:px-6 lg:px-8">
-                            <!-- Currency selector -->
-                            <form>
-                                <div>
-                                    <label for="desktop-currency" class="sr-only">Currency</label>
-                                    <div class="-ml-2 group relative bg-gray-900 border-transparent rounded-md focus-within:ring-2 focus-within:ring-white">
-                                        <select id="desktop-currency" name="currency" class="bg-none bg-gray-900 border-transparent rounded-md py-0.5 pl-2 pr-5 flex items-center text-sm font-medium text-white group-hover:text-gray-100 focus:outline-none focus:ring-0 focus:border-transparent">
-                                            <option>CAD</option>
-
-                                            <option>USD</option>
-
-                                            <option>AUD</option>
-
-                                            <option>EUR</option>
-
-                                            <option>GBP</option>
-                                        </select>
-                                        <div class="absolute right-0 inset-y-0 flex items-center pointer-events-none">
-                                            <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" class="w-5 h-5 text-gray-300">
-                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6 8l4 4 4-4" />
-                                            </svg>
-                                        </div>
-                                    </div>
-                                </div>
-                            </form>
-
-                            <div class="flex items-center space-x-6">
-                                <a href="#" class="text-sm font-medium text-white hover:text-gray-100">Sign in</a>
-                                <a href="#" class="text-sm font-medium text-white hover:text-gray-100">Create an account</a>
-                            </div>
-                        </div>
-                    </div>
-
                     <!-- Secondary navigation -->
                     <div class="backdrop-blur-md backdrop-filter bg-opacity-10 bg-white">
                         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -266,7 +204,7 @@
                                                 <div class="flex">
                                                     <div class="relative flex">
                                                         <button @click="women = true; men = false" @click.away="women = false" type="button" class="relative z-10 flex items-center justify-center transition-colors ease-out duration-200 text-sm font-medium text-white" aria-expanded="false">
-                                                            Women
+                                                            Products
                                                             <!-- Open: "bg-white", Closed: "" -->
                                                             <span class="absolute -bottom-px inset-x-0 h-0.5 transition ease-out duration-200" aria-hidden="true"></span>
                                                         </button>
@@ -341,7 +279,7 @@
                                                 <div class="flex">
                                                     <div class="relative flex">
                                                         <button @click="men = true; women = false" @click.away="men = false" type="button" class="relative z-10 flex items-center justify-center transition-colors ease-out duration-200 text-sm font-medium text-white" aria-expanded="false">
-                                                            Men
+                                                            Education
                                                             <!-- Open: "bg-white", Closed: "" -->
                                                             <span class="absolute -bottom-px inset-x-0 h-0.5 transition ease-out duration-200" aria-hidden="true"></span>
                                                         </button>
@@ -413,9 +351,9 @@
                                                     </div>
                                                 </div>
 
-                                                <a href="#" class="flex items-center text-sm font-medium text-white">Company</a>
+                                                <a href="#" class="flex items-center text-sm font-medium text-white">Support</a>
 
-                                                <a href="#" class="flex items-center text-sm font-medium text-white">Stores</a>
+                                                <a href="#" class="flex items-center text-sm font-medium text-white">About</a>
                                             </div>
                                         </div>
                                     </div>
@@ -423,7 +361,7 @@
                                     <!-- Mobile menu and search (lg-) -->
                                     <div class="flex-1 flex items-center lg:hidden">
                                         <!-- Mobile menu toggle, controls the 'mobileMenuOpen' state. -->
-                                        <button type="button" class="-ml-2 p-2 text-white">
+                                        <button @click="menu = true" type="button" class="-ml-2 p-2 text-white">
                                             <span class="sr-only">Open menu</span>
                                             <!-- Heroicon name: outline/menu -->
                                             <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
@@ -432,8 +370,8 @@
                                         </button>
 
                                         <!-- Search -->
-                                        <a href="#" class="ml-2 p-2 text-white">
-                                            <span class="sr-only">Search</span>
+                                        <a href="{{route('login')}}" class="ml-2 p-2 text-white">
+                                            <span class="sr-only">Login / Register</span>
                                             <!-- Heroicon name: outline/search -->
                                             <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
@@ -448,20 +386,11 @@
                                     </a>
 
                                     <div class="flex-1 flex items-center justify-end">
-                                        <a href="#" class="hidden text-sm font-medium text-white lg:block"> Search </a>
+                                        <x-button href="{{route('login')}}" class="hidden text-sm font-medium text-white lg:block"> Login / Register </x-button>
 
                                         <div class="flex items-center lg:ml-8">
-                                            <!-- Help -->
-                                            <a href="#" class="p-2 text-white lg:hidden">
-                                                <span class="sr-only">Help</span>
-                                                <!-- Heroicon name: outline/question-mark-circle -->
-                                                <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                                </svg>
-                                            </a>
-                                            <a href="#" class="hidden text-sm font-medium text-white lg:block">Help</a>
-
                                             <!-- Cart -->
+                                            @auth()
                                             <div class="ml-4 flow-root lg:ml-8">
                                                 <a href="#" class="group -m-2 p-2 flex items-center">
                                                     <!-- Heroicon name: outline/shopping-bag -->
@@ -472,6 +401,7 @@
                                                     <span class="sr-only">items in cart, view bag</span>
                                                 </a>
                                             </div>
+                                            @endauth()
                                         </div>
                                     </div>
                                 </div>
@@ -482,7 +412,7 @@
             </header>
 
             <div class="relative max-w-3xl mx-auto py-32 px-6 flex flex-col items-center text-center sm:py-64 lg:px-0">
-                <h1 class="text-4xl font-extrabold tracking-tight text-white lg:text-6xl">New arrivals are here</h1>
+                <h1 class="text-4xl font-extrabold tracking-tight text-white lg:text-6xl">Introducing Student Shop</h1>
                 <p class="mt-4 text-xl text-white">The new arrivals have, well, newly arrived. Check out the latest options from our summer small-batch release while they're still in stock.</p>
                 <a href="#" class="mt-8 inline-block bg-white border border-transparent rounded-md py-3 px-8 text-base font-medium text-gray-900 hover:bg-gray-100">Shop New Arrivals</a>
             </div>
