880e9fd76e58dc7c3ea67597e9b63b4ed7ee1a3f
Will <twitchifygamers@gmail.com>
2022-05-05 17:37:44 +0100

    Created factory for reviews

---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use App\Models\Review;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Review>
+ */
+class ReviewFactory extends Factory
+{
+    protected $model = Review::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::query()->get('id')->random(),
+            'product_id' => Product::query()->get('id')->random(),
+            'review' => $this->faker->sentence()
+        ];
+    }
+}
